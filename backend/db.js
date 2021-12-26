432282879a5d979ed1d01bd48db144e5f4e57861
ShubhamK <54986627+shubhamdathia7257@users.noreply.github.com>
2021-12-26 19:25:55 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const url = "mongodb://localhost/socialqa";
+const url = "mongodb+srv://skd7257:TCToCxok8d0Yp1vF@cluster0.kgeey.mongodb.net/SocialQA?retryWrites=true&w=majority";
 module.exports.connect = () => {
   mongoose
     .connect(url, {
